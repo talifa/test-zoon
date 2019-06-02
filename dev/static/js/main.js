73375b7b9397a9bd1ab0cc0a78c15341bbfbcfa0
talifa <talifaidk@gmail.com>
2019-06-02 13:18:25 +0300

    bem mobile

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
-    svg4everybody({});
+$(document).ready(function() {
+  svg4everybody({});
+});
+jQuery(function($) {
+  $(document).mouseup(function(e) {
+    var div = $(".row-menu__list");
+    if (!div.is(e.target) && div.has(e.target).length === 0) {
+      div.removeClass("d-block");
+    }
+    $(".menu").click(function() {
+      $(this)
+        .children(".row-menu__list")
+        .toggleClass("d-block");
+    });
+  });
 });
